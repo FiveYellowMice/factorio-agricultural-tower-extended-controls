ae3d61bd48bf0ddbaa835c3b1e885fc0cd878ecc
FiveYellowMice <hkz85825915@gmail.com>
2025-09-18 21:57:25 +1000

    Rename "extended signals" to "extended controls"

--- constants.lua
+++ constants.lua
@@ -1,8 +1,10 @@
 local constants = {}
 
-constants.output_combinator_name = "extended_agricultural_tower_signals-output-combinator"
-constants.gui_name = "extended_agricultural_tower_signals"
-constants.gui_changed_event_enabled = "extended_agricultural_tower_signals-changed-event-enabled"
-constants.gui_style_prefix = "extended_agricultural_tower_signals-"
+constants.name_prefix = "agricultural_tower_extended_controls-"
+
+constants.output_combinator_name = constants.name_prefix.."output-combinator"
+constants.gui_name = constants.name_prefix.."tower-gui"
+constants.gui_changed_event_enabled = constants.name_prefix.."changed-event-enabled"
+constants.gui_style_prefix = constants.name_prefix
 
 return constants
